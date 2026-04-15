a1285844574d25befc11ac5a7a30b1cbefeba927
jawad zafari <jawad.zafari@laplateforme.io>
2026-04-16 00:33:58 +0200

    ajouter de series

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,10 @@
 export interface Movie {
     id: number;
-    title: string;
+    title: string; //films
+    name?: string;  //séries
     poster_path: string;
     overview: string;
-    release_date?: string;
+    release_date?: string; //  films
+    first_air_date?: string; // séries
+    backdrop_path?: string; 
 }
