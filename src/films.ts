47856f10e9e1c6497baab5b23e7c893a8809ea88
jawad zafari <jawad.zafari@laplateforme.io>
2026-04-22 10:32:31 +0200

    des variables et const

---
+++ src/films.ts
@@ -0,0 +1,11 @@
+import { API_KEY, BASE_URL, IMAGE_BASE_URL } from './config.js';
+
+// Pagination
+let currentPage = 1;
+let totalPages = 1;
+
+// Éléments du DOM
+const grid = document.getElementById('catalog-grid');
+const btnPrev = document.getElementById('btn-prev') as HTMLButtonElement;
+const btnNext = document.getElementById('btn-next') as HTMLButtonElement;
+const pageIndicator = document.getElementById('page-indicator');
