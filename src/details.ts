c6212427233ca47f3c7c6f6e2df5f9a65d74d063
jawad zafari <jawad.zafari@laplateforme.io>
2026-04-22 10:47:06 +0200

    const essentielles

--- src/details.ts
+++ src/details.ts
@@ -1,1 +1,6 @@
 import { API_KEY, BASE_URL, IMAGE_BASE_URL } from './config.js';
+
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
+const type = urlParams.get('type') || 'movie'; 
+
