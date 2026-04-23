ead4b8fca76984e5141ab9b61c4a1e21f2fd6ec3
jawad zafari <jawad.zafari@laplateforme.io>
2026-04-23 14:41:15 +0200

    fonction de gestion des commentaires

--- src/comments.ts
+++ src/comments.ts
@@ -1,1 +1,14 @@
 import { API_KEY, BASE_URL } from "./config.js";
+
+/**
+ * La fonction principale de gestion des commentaires
+ */
+export async function initComments(id: string, type: string): Promise<void> {
+  const commentsList = document.getElementById("comments-list");
+  const commentForm = document.getElementById(
+    "comment-form",
+  ) as HTMLFormElement;
+
+  if (!commentsList || !commentForm) return;
+
+}
