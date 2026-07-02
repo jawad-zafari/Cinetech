bca8040ec0800d10186d61a4b565d29356c4ba79
jawad zafari <jawad.zafari@laplateforme.io>
2026-07-02 13:37:21 +0200

    Ajouter  la récupération du terme recherché depuis l'URL

--- src/searchResults.ts
+++ src/searchResults.ts
@@ -1,2 +1,10 @@
 import { API_KEY, BASE_URL, IMAGE_BASE_URL } from './config.js';
 
+// Récupération du terme recherché depuis l'URL
+const urlParams = new URLSearchParams(window.location.search);
+const query = urlParams.get('q');
+
+const searchTitle = document.getElementById('search-title');
+const resultsGrid = document.getElementById('search-results-grid');
+const noResultsMsg = document.getElementById('no-results-message');
+
